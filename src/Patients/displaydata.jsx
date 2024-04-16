f9b22f50bd458edcd29f4a0b5e4fb65c7e5d7c6a
Vanessa <vanessarodrigues010506@gmail.com>
2024-04-16 06:52:05 +0530

    displaydata base logic

--- src/Patients/displaydata.jsx
+++ src/Patients/displaydata.jsx
@@ -1,36 +1,49 @@
+import React, { useEffect, useState } from 'react';
+import { db } from '../connection';
+import 'firebase/firestore';
+
 const DisplayData = () => {
-  var Image;
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    // Fetch data from Firestore
+    const fetchData = async () => {
+      try {
+        const snapshot = await db.collection('data').get();
+        const fetchedData = snapshot.docs.map((doc) => doc.data());
+        setData(fetchedData);
+      } catch (error) {
+        console.error('Error fetching data: ', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <div>
-      <h1>Medical history</h1>
-
-      <div className="col-md-4">
-        <label
-          for="validationDefault01"
-          className="form-label">
-          medical report
-        </label>
-        <input
-          type="file"
-          filename={Image}
-          className="form-control"
-          aria-label="X-ray Scan"
-          required
-        />
-      </div>
-      <img
-        src="../ftsqfnvf.png"
-        alt="ssor"
-      />
-      <img
-        src="https://i.ibb.co/VHpNy69/B-10276-9.png"
-        alt="B-10276-9"
-        border="0"></img>
-      <img
-        src={"https://i.ibb.co/7y6qZ3Y/" + Image + ".png"}
-        alt="ftsqfnvf"
-        border="0"></img>
+      <table>
+        <thead>
+          <tr>
+            <th>Field 1</th>
+            <th>Field 2</th>
+            <th>Field 3</th>
+            {/* Add more table headers as needed */}
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((item, index) => (
+            <tr key={index}>
+              <td>{item.field1}</td>
+              <td>{item.field2}</td>
+              <td>{item.field3}</td>
+              {/* Add more table cells as needed */}
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 };
+
 export default DisplayData;
